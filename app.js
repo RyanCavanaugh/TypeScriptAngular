c334cba653e34dfc6560240bab49cb33dfe75641
Ryan Cavanaugh <ryan.cavanaugh@microsoft.com>
2014-10-15 14:54:08 -0700

    Fix casing; move into separate function

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 /// <reference path="angular.d.ts" />
 /// <reference path="octokit.d.ts" />
-angular.module('sampleApp', []).controller('commitList', function ($http) {
+function commitListController($http) {
     $http.jsonp('https://api.github.com/repos/Microsoft/TypeScript/commits?callback=JSON_CALLBACK').success(function (data) {
         var descriptions = data.data.map(function (c) {
             return c.commit.message;
         });
         document.getElementById('results').innerHTML = descriptions.join('<br>');
     });
-});
+}
+
+angular.module('sampleApp', []).controller('CommitList', commitListController);
--- app.ts
+++ app.ts
@@ -1,11 +1,13 @@
 /// <reference path="angular.d.ts" />
 /// <reference path="octokit.d.ts" />
 
+function commitListController($http: ng.IHttpService) {
+    $http.jsonp<CommitData>('https://api.github.com/repos/Microsoft/TypeScript/commits?callback=JSON_CALLBACK').success(data => {
+        var descriptions: string[] = data.data.map(c => c.commit.message);
+        document.getElementById('results').innerHTML = descriptions.join('<br>');
+    });
+}
+
 angular.module('sampleApp', [])
-    .controller('commitList', function ($http: ng.IHttpService) {
-        $http.jsonp<CommitData>('https://api.github.com/repos/Microsoft/TypeScript/commits?callback=JSON_CALLBACK').success(data => {
-            var descriptions: string[] = data.data.map(c => c.commit.message);
-            document.getElementById('results').innerHTML = descriptions.join('<br>');
-        });
-});
+    .controller('CommitList', commitListController);
 
