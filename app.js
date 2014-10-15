b059c66f5b11c5ff7431b15acff85fe4ec038f4c
Ryan Cavanaugh <ryan.cavanaugh@microsoft.com>
2014-10-15 14:12:37 -0700

    Barebones example: Load raw JSON from GitHub API

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 /// <reference path="angular.d.ts" />
+function jsonp(url) {
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = url;
+    document.head.appendChild(script);
+}
+
+function acceptData(data) {
+    document.getElementById('results').innerText = JSON.stringify(data);
+}
+
+window.onload = function () {
+    var url = 'https://api.github.com/repos/Microsoft/TypeScript/commits?callback=acceptData';
+    jsonp(url);
+};
--- app.ts
+++ app.ts
@@ -1,1 +1,18 @@
 /// <reference path="angular.d.ts" />
+
+function jsonp(url: string) {
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = url;
+    document.head.appendChild(script);
+}
+
+function acceptData(data) {
+    document.getElementById('results').innerText = JSON.stringify(data);
+}
+
+window.onload = function () {
+    var url = 'https://api.github.com/repos/Microsoft/TypeScript/commits?callback=acceptData';
+    jsonp(url);
+}
+
