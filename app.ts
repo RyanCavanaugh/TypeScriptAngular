e2c7f4c0ea51a2f27533de3888414281d2dbb473
Ryan Cavanaugh <ryanca@microsoft.com>
2014-10-21 13:03:32 -0700

    4. Use angular's $http service

--- app.ts
+++ app.ts
@@ -4,9 +4,9 @@
 
 // "https://api.github.com/repos/Microsoft/TypeScript/issues?callback=?"
 
-angular.module('myApp', []).controller('CommitController', () => {
-    var commitUrl = "https://api.github.com/repos/Microsoft/TypeScript/commits?callback?";
-    $.getJSON(commitUrl, (data: Octokit.Commit[]) => {
-        $('#commits').html(data.map(c => c.commit.message).join('<br>'));
-    });
+angular.module('myApp', []).controller('CommitController', ($http: ng.IHttpService) => {
+    $http.jsonp("https://api.github.com/repos/Microsoft/TypeScript/commits?callback=JSON_CALLBACK")
+        .success((data: Octokit.CommitQuery) => {
+            $('#commits').html(data.data.map(c => c.commit.message).join('<br>'));
+      });
 });
