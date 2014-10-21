1b4407197b7240fc0bbd20ef8a1adf5b68a80b79
Ryan Cavanaugh <ryanca@microsoft.com>
2014-10-21 12:29:29 -0700

    1. Basic display of raw JSON data using jQuery.ajax

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 /// <reference path="jquery.d.ts" />
 // "https://api.github.com/repos/Microsoft/TypeScript/issues?callback=?"
-// "https://api.github.com/repos/Microsoft/TypeScript/commits?callback?"
+$(function () {
+    var commitUrl = "https://api.github.com/repos/Microsoft/TypeScript/commits?callback?";
+    $.getJSON(commitUrl, function (data) {
+        $('#commits').html(JSON.stringify(data));
+    });
+});
