0e9ce228236ca151e13a3b64f7452434540d476f
Ryan Cavanaugh <ryanca@microsoft.com>
2014-10-21 13:06:07 -0700

    5. Use angular's $scope for data binding

--- app.js
+++ app.js
@@ -2,10 +2,10 @@
 /// <reference path="angular.d.ts" />
 /// <reference path="octokit.d.ts" />
 // "https://api.github.com/repos/Microsoft/TypeScript/issues?callback=?"
-angular.module('myApp', []).controller('CommitController', function ($http) {
+angular.module('myApp', []).controller('CommitController', function ($http, $scope) {
     $http.jsonp("https://api.github.com/repos/Microsoft/TypeScript/commits?callback=JSON_CALLBACK").success(function (data) {
-        $('#commits').html(data.data.map(function (c) {
-            return c.commit.message;
-        }).join('<br>'));
+        $scope.commits = data.data.map(function (c) {
+            return c.commit;
+        });
     });
 });
