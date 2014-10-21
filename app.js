d67e01fae4896e8dc7f719f8086af123daeb69a0
Ryan Cavanaugh <ryanca@microsoft.com>
2014-10-21 12:27:00 -0700

    Initial files

---
+++ app.js
@@ -0,0 +1,3 @@
+/// <reference path="jquery.d.ts" />
+// "https://api.github.com/repos/Microsoft/TypeScript/issues?callback=?"
+// "https://api.github.com/repos/Microsoft/TypeScript/commits?callback?"
