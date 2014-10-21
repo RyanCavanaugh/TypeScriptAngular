fdcab1b3def3fd251e3dd9f41d6068f9fa60f65f
Ryan Cavanaugh <ryanca@microsoft.com>
2014-10-21 13:15:45 -0700

    7. Add a filter

--- app.js
+++ app.js
@@ -2,6 +2,16 @@
 /// <reference path="angular.d.ts" />
 /// <reference path="octokit.d.ts" />
 // "https://api.github.com/repos/Microsoft/TypeScript/issues?callback=?"
+function limitFactory() {
+    return function (s) {
+        if (s.length > 80) {
+            return s.substr(0, 77) + '...';
+        } else {
+            return s;
+        }
+    };
+}
+
 angular.module('myApp', []).controller('CommitController', function ($http, $scope) {
     $http.jsonp("https://api.github.com/repos/Microsoft/TypeScript/commits?callback=JSON_CALLBACK").success(function (data) {
         $scope.commits = data.data.map(function (c) {
@@ -12,4 +22,4 @@ angular.module('myApp', []).controller('CommitController', function ($http, $sco
     $http.jsonp("https://api.github.com/repos/Microsoft/TypeScript/issues?callback=JSON_CALLBACK").success(function (data) {
         $scope.issues = data.data;
     });
-});
+}).filter('limit', limitFactory);
