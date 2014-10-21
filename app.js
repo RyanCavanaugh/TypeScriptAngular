485ab1971c29806b0bcebdef3e745d0687f52039
Ryan Cavanaugh <ryanca@microsoft.com>
2014-10-21 12:30:34 -0700

    2. Display formatted data with type annotations

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 /// <reference path="jquery.d.ts" />
+/// <reference path="octokit.d.ts" />
 // "https://api.github.com/repos/Microsoft/TypeScript/issues?callback=?"
 $(function () {
     var commitUrl = "https://api.github.com/repos/Microsoft/TypeScript/commits?callback?";
     $.getJSON(commitUrl, function (data) {
-        $('#commits').html(JSON.stringify(data));
+        $('#commits').html(data.map(function (c) {
+            return c.commit.message;
+        }).join('<br>'));
     });
 });
--- app.ts
+++ app.ts
@@ -1,10 +1,11 @@
 /// <reference path="jquery.d.ts" />
+/// <reference path="octokit.d.ts" />
 
 // "https://api.github.com/repos/Microsoft/TypeScript/issues?callback=?"
 
 $(() => {
     var commitUrl = "https://api.github.com/repos/Microsoft/TypeScript/commits?callback?";
-    $.getJSON(commitUrl, (data) => {
-        $('#commits').html(JSON.stringify(data));
+    $.getJSON(commitUrl, (data: Octokit.Commit[]) => {
+        $('#commits').html(data.map(c => c.commit.message).join('<br>'));
     });
 });
