873031a91e12a00cc588a2e920873ba35cf0dfde
Ryan Cavanaugh <ryanca@microsoft.com>
2014-10-21 13:10:16 -0700

    6. Add another controller

--- app.ts
+++ app.ts
@@ -4,9 +4,16 @@
 
 // "https://api.github.com/repos/Microsoft/TypeScript/issues?callback=?"
 
-angular.module('myApp', []).controller('CommitController', ($http: ng.IHttpService, $scope: any) => {
-    $http.jsonp("https://api.github.com/repos/Microsoft/TypeScript/commits?callback=JSON_CALLBACK")
-        .success((data: Octokit.CommitQuery) => {
-            $scope.commits = data.data.map(c => c.commit);
-      });
-});
+angular.module('myApp', [])
+    .controller('CommitController', ($http: ng.IHttpService, $scope: any) => {
+        $http.jsonp("https://api.github.com/repos/Microsoft/TypeScript/commits?callback=JSON_CALLBACK")
+            .success((data: Octokit.CommitQuery) => {
+                $scope.commits = data.data.map(c => c.commit);
+         });
+    })
+    .controller('IssueController', ($http: ng.IHttpService, $scope: any) => {
+        $http.jsonp("https://api.github.com/repos/Microsoft/TypeScript/issues?callback=JSON_CALLBACK")
+            .success((data: Octokit.IssueQuery) => {
+                $scope.issues = data.data;
+            });
+    })
